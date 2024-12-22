e6821772c07624c82f38034417bd11678119f2c8
VK132211 <m.vk.80899@gmail.com>
2024-12-22 14:03:04 +0530

    Add project setup with configuration, routes, and schemas

---
+++ src/routes/v1/v1Router.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import channelRouter from './channel.js';
+import memberRouter from './members.js';
+import userRouter from './users.js';
+import workspaceRouter from './workspaces.js';
+const router = express.Router();
+
+router.use('/users', userRouter);
+
+router.use('/workspaces', workspaceRouter);
+
+router.use('/channels', channelRouter);
+
+router.use('/members', memberRouter);
+
+export default router;
